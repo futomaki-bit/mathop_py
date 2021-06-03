b59fdd946bf5ce1d5732287811903dc314261b00
futomakiyoin <futomakiyoin@gmail.com>
2021-06-03 15:42:27 -0400

    Initial Release

---
+++ main.py
@@ -0,0 +1,52 @@
+# import time for timer, random for generating array to test
+import time
+import random
+
+a = 11111
+b = 333
+# quicksort breaks with this
+# ["quicksort",quick_sort(array)]
+
+
+def multiplication(first, second):
+    return first*second
+
+
+def summation(first, second):
+    return first+second
+
+# NOTE: ^ IS NOT POWER/EXPONENTIAL, IT IS XOR
+
+
+def power(first, second):
+    return first**second
+
+
+def subtraction(first, second):
+    return first - second
+
+
+def division(first, second):
+    return first/second
+
+
+def remainder(first, second):
+    return first % second
+
+
+# Edit list here
+funcList = [
+    ["+", summation],
+    ["x", multiplication],
+    ["exp", power],
+    ["-", subtraction],
+    ["/", division],
+    ["%", remainder]
+]
+
+# driver to run all the algorithms in the list with the same array
+for f in funcList:
+    start_time = time.time()
+    time.sleep(1)
+    f[1](a, b)
+    print('{:<5}  {:<}'.format(f[0], (time.time() - start_time - 1)))
